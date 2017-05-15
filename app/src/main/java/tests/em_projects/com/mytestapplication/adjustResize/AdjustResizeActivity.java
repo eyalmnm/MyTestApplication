74fba05b7a104707b7c0ae8d2c65d6e99ce20a01
eyal_listenapp <eyal.listenapp.net[credential]>
2017-05-15 17:18:19 +0300

    This Commit includes:  - Add AdjustResizeActivity

--- app/src/main/java/tests/em_projects/com/mytestapplication/adjustResize/AdjustResizeActivity.java
+++ app/src/main/java/tests/em_projects/com/mytestapplication/adjustResize/AdjustResizeActivity.java
@@ -3,6 +3,7 @@ package tests.em_projects.com.mytestapplication.adjustResize;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ScrollView;
 
 import tests.em_projects.com.mytestapplication.R;
 
@@ -17,5 +18,7 @@ public class AdjustResizeActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adjust_resize);
+        ScrollView sv = (ScrollView) findViewById(R.id.smsAuthScroll);
+        sv.setEnabled(false);
     }
 }
