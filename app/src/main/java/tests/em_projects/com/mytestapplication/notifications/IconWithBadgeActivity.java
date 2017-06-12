4224e43561edc24b8c37590590816fa118fb3737
eyal_listenapp <eyal.listenapp.net[credential]>
2017-06-12 18:11:43 +0300

    This Commit includes:  - Add the ability to run it with programable layout

--- app/src/main/java/tests/em_projects/com/mytestapplication/notifications/IconWithBadgeActivity.java
+++ app/src/main/java/tests/em_projects/com/mytestapplication/notifications/IconWithBadgeActivity.java
@@ -1,11 +1,16 @@
 package tests.em_projects.com.mytestapplication.notifications;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import tests.em_projects.com.mytestapplication.R;
+import tests.em_projects.com.mytestapplication.utils.DimenUtils;
 
 /**
  * Created by USER on 06/06/2017.
@@ -18,9 +23,9 @@ public class IconWithBadgeActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_icon_with_badge);
-
-        iconWithBadge = (IconWithBadge) findViewById(R.id.iconWithBadge);
+        setContentView(getView());
+//        setContentView(R.layout.activity_icon_with_badge);
+//        iconWithBadge = (IconWithBadge) findViewById(R.id.iconWithBadge);
 
         iconWithBadge.setIcon(R.drawable.listenapp);
 
@@ -47,5 +52,23 @@ public class IconWithBadgeActivity extends Activity {
             }
         }, 6000);
 
+    }
+
+    private View getView() {
+        LinearLayout mainView = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mainView.setOrientation(LinearLayout.HORIZONTAL);
+        mainView.setLayoutParams(layoutParams);
+        int padding = DimenUtils.dpToPx(5);
+        mainView.setPadding(padding, padding, padding, padding);
+        mainView.setBackgroundColor(Color.YELLOW);
+
+        iconWithBadge = new IconWithBadge(this);
+        int dim = DimenUtils.dpToPx(40);
+        LinearLayout.LayoutParams iconParams = new LinearLayout.LayoutParams(dim, dim);
+        mainView.addView(iconWithBadge, iconParams);
+
+        return mainView;
     }
 }
