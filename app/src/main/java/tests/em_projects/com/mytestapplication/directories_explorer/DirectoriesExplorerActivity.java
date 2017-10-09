eb7ac8afee4a84dda2ae9911cd1593d6378c96bc
eyalmnm <eyal@em-projects.com>
2017-10-09 14:12:16 +0300

    This Commit includes:  - Add File.separator to Directories Explorer.

--- app/src/main/java/tests/em_projects/com/mytestapplication/directories_explorer/DirectoriesExplorerActivity.java
+++ app/src/main/java/tests/em_projects/com/mytestapplication/directories_explorer/DirectoriesExplorerActivity.java
@@ -18,9 +18,11 @@ import tests.em_projects.com.mytestapplication.R;
 // requires "android.permission.READ_EXTERNAL_STORAGE" permissions
 
 public class DirectoriesExplorerActivity extends Activity {
-    public static final String BASE_PATH = Environment.getExternalStorageDirectory().toString();
+
+    public static final String BASE_PATH = Environment.getExternalStorageDirectory().toString() + "/viber";
     private static final String TAG = "DirectoriesExplorerAct";
     private TextView outputTextView;
+    private String fSa = File.separator;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,17 +32,17 @@ public class DirectoriesExplorerActivity extends Activity {
         outputTextView = (TextView) findViewById(R.id.outputTextView);
 
         File baseDir = new File(BASE_PATH);
-        findDirctories(baseDir, BASE_PATH);
+        findDirctories(baseDir, "");
     }
 
     private void findDirctories(File dir, String name) {
         if (true == dir.isDirectory()) {
             outputTextView.append(name + "\n");
-            Log.d(TAG, "dir name: " + name);
+            Log.d(TAG, "dir name: " + name + "[" + dir.getPath() + "]");
             File[] files = dir.listFiles();
             if (null == files || 0 == files.length) return;
             for (File file : files) {
-                findDirctories(file, name + "/" + file.getName());
+                findDirctories(file, name + fSa + file.getName());
             }
         }
     }
