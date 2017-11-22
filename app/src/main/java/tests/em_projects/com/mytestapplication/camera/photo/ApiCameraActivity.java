3cbf77c82d3340253fcc555efe3bc0cc2116bec3
eyalmnm <eyal@em-projects.com>
2017-11-22 11:41:07 +0200

    This Commit includes:  - Add Camera Demo.

---
+++ app/src/main/java/tests/em_projects/com/mytestapplication/camera/photo/ApiCameraActivity.java
@@ -0,0 +1,273 @@
+package tests.em_projects.com.mytestapplication.camera.photo;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import tests.em_projects.com.mytestapplication.R;
+
+/**
+ * Created by eyalmuchtar on 10/31/17.
+ */
+
+public class ApiCameraActivity extends Activity {
+    private static final String TAG = "ApiCameraActivity";
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private PictureCallback mPicture;
+    OnClickListener captrureListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mCamera.takePicture(null, null, mPicture);
+        }
+    };
+    private Button capture, switchCamera;
+    private Context myContext;
+    private LinearLayout cameraPreview;
+    private boolean cameraFront = false;
+    OnClickListener switchCameraListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //get the number of cameras
+            int camerasNumber = Camera.getNumberOfCameras();
+            if (camerasNumber > 1) {
+                //release the old camera instance
+                //switch camera, from the front and the back and vice versa
+
+                releaseCamera();
+                chooseCamera();
+            } else {
+                Toast toast = Toast.makeText(myContext, "Sorry, your phone has only one camera!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    };
+
+    //make picture and save to a folder
+    private static File getOutputMediaFile() {
+        //make a new file directory inside the "sdcard" folder
+        File path = Environment.getExternalStorageDirectory();
+        String save_path = path.toString() + File.separator + "mytestappcamera";
+        File mediaStorageDir = new File(save_path);
+        //if this "MyTestAppCamera" folder does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        //take the current timeStamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        //and make a media file:
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_camera);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+        initialize();
+    }
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        try {
+            int numberOfCameras = Camera.getNumberOfCameras();
+            for (int i = 0; i < numberOfCameras; i++) {
+                CameraInfo info = new CameraInfo();
+                Camera.getCameraInfo(i, info);
+                if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+                    cameraId = i;
+                    cameraFront = true;
+                    break;
+                }
+            }
+        } catch (RuntimeException e) {
+            Log.e(TAG, "findFrontFacingCamera", e);
+        }
+        return cameraId;
+    }
+
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        //Search for the back facing camera
+        //get the number of cameras
+        int numberOfCameras = Camera.getNumberOfCameras();
+        //for every camera check
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                cameraFront = false;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    public void onResume() {
+        super.onResume();
+        if (!hasCamera(myContext)) {
+            Toast toast = Toast.makeText(myContext, "Sorry, your phone does not have a camera!", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "onResume -> Missing permissions");
+            Toast.makeText(myContext, "Turn on Camera permissions manually", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
+        } else {
+            openCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
+        }
+    }
+
+    private void openCamera() {
+        if (mCamera == null) {
+            //if the front facing camera does not exist
+            if (findFrontFacingCamera() < 0) {
+                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+                switchCamera.setVisibility(View.GONE);
+            }
+            int cameraId = findBackFacingCamera();
+            try {
+                mCamera = Camera.open(cameraId);
+            } catch (RuntimeException e) {
+                Log.e(TAG, "onResume", e);
+            }
+            mPicture = getPictureCallback();
+            mPreview.refreshCamera(mCamera);
+        }
+    }
+
+    public void initialize() {
+        cameraPreview = (LinearLayout) findViewById(R.id.camera_preview);
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
+        capture = (Button) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captrureListener);
+
+        switchCamera = (Button) findViewById(R.id.button_ChangeCamera);
+        switchCamera.setOnClickListener(switchCameraListener);
+    }
+
+    public void chooseCamera() {
+        //if the camera preview is the front
+        if (cameraFront) {
+            int cameraId = findBackFacingCamera();
+            if (cameraId >= 0) {
+                //open the backFacingCamera
+                //set a picture callback
+                //refresh the preview
+
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        } else {
+            int cameraId = findFrontFacingCamera();
+            if (cameraId >= 0) {
+                //open the backFacingCamera
+                //set a picture callback
+                //refresh the preview
+
+                mCamera = Camera.open(cameraId);
+                mPicture = getPictureCallback();
+                mPreview.refreshCamera(mCamera);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //when on Pause, release camera in order to be used from other applications
+        releaseCamera();
+    }
+
+    private boolean hasCamera(Context context) {
+        //check if the device has camera
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private PictureCallback getPictureCallback() {
+        PictureCallback picture = new PictureCallback() {
+
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                //make a new picture file
+                File pictureFile = getOutputMediaFile();
+
+                if (pictureFile == null) {
+                    return;
+                }
+                try {
+                    //write the file
+                    FileOutputStream fos = new FileOutputStream(pictureFile);
+                    fos.write(data);
+                    fos.close();
+                    Toast toast = Toast.makeText(myContext, "Picture saved: " + pictureFile.getName(), Toast.LENGTH_LONG);
+                    toast.show();
+
+                } catch (FileNotFoundException e) {
+                } catch (IOException e) {
+                }
+
+                //refresh camera to continue preview
+                mPreview.refreshCamera(mCamera);
+            }
+        };
+        return picture;
+    }
+
+    private void releaseCamera() {
+        // stop and release camera
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+}
