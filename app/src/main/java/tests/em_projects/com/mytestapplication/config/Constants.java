6a8a5589c3a1c27cd546a1923ba2bafbb2c5bcad
eyalmnm <eyal@em-projects.com>
2017-11-26 12:18:43 +0200

    This Commit includes:  - Add BASE_PATH constant.

---
+++ app/src/main/java/tests/em_projects/com/mytestapplication/config/Constants.java
@@ -0,0 +1,11 @@
+package tests.em_projects.com.mytestapplication.config;
+
+import android.os.Environment;
+
+/**
+ * Created by eyalmuchtar on 11/25/17.
+ */
+
+public class Constants {
+    public static final String BASE_PATH = Environment.getExternalStorageDirectory().toString();
+}
